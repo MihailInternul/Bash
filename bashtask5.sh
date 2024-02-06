77c82e7a5cf2af392fc93bbb4c898ffe45178b91
MihailInternul <157023284+MihailInternul@users.noreply.github.com>
2024-02-06 11:10:36 +0200

    bashtask5.sh

--- bashtask5.sh
+++ bashtask5.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Write script with following functionality:
+# If -v tag is passed, replaces lowercase characters with uppercase and vise versa
+# If -s is passed, script substitutes <A_WORD> with <B_WORD> in text (case sensitive)
+# If -r is passed, script reverses text lines
+# If -l is passed, script converts all the text to lower case
+# If -u is passed, script converts all the text to upper case
+# Script should work with -i <input file> -o <output file> tags
+
 while getopts ":i:o:vsrlu" opt; do
   case $opt in
     i)

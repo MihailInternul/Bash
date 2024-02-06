45cadb9552943c7d05633a5bb738a7a52ab65eeb
MihailInternul <157023284+MihailInternul@users.noreply.github.com>
2024-02-06 11:08:53 +0200

    bashtask3.sh

--- bashtask3.sh
+++ bashtask3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# You need to write a script that prints the numbers from 1 to 100 such that:
+# If the number is a multiple of 3, you need to print "Fizz" instead of that number.
+# If the number is a multiple of 5, you need to print "Buzz" instead of that number.
+# If the number is a multiple of both 3 and 5, you need to print "FizzBuzz" instead of that number.
+
 for ((i=1; i<=100; i++)); do
     # Check for multiples of 3 and 5 first
     if ((i % 3 == 0)) && ((i % 5 == 0)); then
